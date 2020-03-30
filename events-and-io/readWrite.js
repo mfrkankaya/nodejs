fe7630d4a2b512c72244d0a70bc213256a84a64f
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-30 21:18:38 +0300

    Read, Write and Remove Files completed

--- events-and-io/readWrite.js
+++ events-and-io/readWrite.js
@@ -1,5 +1,24 @@
 const fs = require('fs')
 
+/* ------------------------------- Write File ------------------------------- */
+
+// fs.appendFile() -> Adds new data to existing one
+// fs.writeFile() -> Removes old content and writes new one
+
+fs.appendFile('demo.txt', 'new content', err => {
+  if (err) throw err
+
+  console.log('content added')
+})
+
+fs.writeFile('demo.txt', 'new content', err => {
+  if (err) throw err
+
+  console.log('content added')
+})
+
+/* -------------------------------- Read File ------------------------------- */
+
 fs.readFile('demo.txt', (err, data) => {
   if (err) console.error(err)
   console.log(data.toString())
@@ -7,3 +26,11 @@ fs.readFile('demo.txt', (err, data) => {
 
 const demo = fs.readFileSync('demo.txt')
 console.log(demo.toString())
+
+/* ------------------------------- Remove File ------------------------------ */
+
+fs.unlink('demo.txt', err => {
+  if (err) throw err
+
+  console.log('File removed.')
+})
