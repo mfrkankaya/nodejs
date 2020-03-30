4b34196664f2ea1ba9a3a49f45e1c44e965105c4
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-30 21:36:45 +0300

    Simple routing

--- http/httpserver.js
+++ http/httpserver.js
@@ -1,12 +1,23 @@
 const http = require('http')
 const fs = require('fs')
 
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'content-type': 'text/html; charset=utf8' })
+//   fs.readFile('index.html', (err, data) => {
+//     if (err) throw err
+//     res.end(data)
+//   })
+// })
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html; charset=utf8' })
-  fs.readFile('index.html', (err, data) => {
-    if (err) throw err
-    res.end(data)
-  })
+  if (req.method === 'GET') {
+    if (req.url === '/') res.write("You're home.")
+    else if (req.url === '/contact') res.write("Here's contact.")
+    else res.write('Not found.')
+  }
+
+  res.end()
 })
 
 server.listen(3000)
