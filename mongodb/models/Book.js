9ac29e20ebd4d8c588bd8c88152bbc57d5266f81
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-31 00:22:26 +0300

    Mongo Schema - Type, Default, Unique, Required

--- mongodb/models/Book.js
+++ mongodb/models/Book.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const BookSchema = new Schema({
-  title: String,
-  published: Boolean,
+  title: { type: String, required: true },
+  published: { type: Boolean, default: false, unique: true },
   comments: [{ message: String }],
   meta: {
     votes: Number,
     favs: Number
+  },
+  publishedAt: {
+    type: Date,
+    default: Date.now
   }
 })
 
