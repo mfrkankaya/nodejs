09db320959ed0b8d2428f7c537c6d4af1aefb4d4
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-30 22:08:11 +0300

    Add parameters to pug file

--- express/app.js
+++ express/app.js
@@ -4,7 +4,7 @@ const app = express()
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { name: 'Furkan', surname: 'Kaya' })
 })
 
 app.listen(3000, () => console.log('Server up!'))
