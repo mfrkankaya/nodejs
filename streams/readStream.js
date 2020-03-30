0fbf039394de4bcd48d509bdf55226e4f9532c2d
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-30 21:51:53 +0300

    Create Write Stream

--- streams/readStream.js
+++ streams/readStream.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const file = 'video.mp4'
 
 const readStream = fs.createReadStream(file)
+const writeStream = fs.createWriteStream('new.mp4')
 
 fs.stat(file, (err, data) => {
   let progress = 0
@@ -11,6 +12,11 @@ fs.stat(file, (err, data) => {
   readStream.on('data', chunk => {
     progress += chunk.length
     console.log(`${Math.round((100 * progress) / total)}%`)
+  })
+
+  readStream.pipe(writeStream)
+  writeStream.on('finish', () => {
+    console.log('New file created.')
   })
 
   readStream.on('end', () => {
