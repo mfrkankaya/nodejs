07bdfb60eff5276642a5fe823d854724b8f22fc3
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-30 21:08:43 +0300

    __filename and __dirname

---
+++ events-and-io/filenameAndDirname.js
@@ -0,0 +1,2 @@
+console.log(__filename) // /Users/muhammedfurkankaya/lessons/nodejs/events-and-io/filenameAndDirname.js
+console.log(__dirname) // /Users/muhammedfurkankaya/lessons/nodejs/events-and-io/
