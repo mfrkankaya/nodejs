6b4a6103d5675500f9d84e54985d0679504bc92f
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-30 22:28:22 +0300

    Pug Layout and Block System

--- express/app.js
+++ express/app.js
@@ -7,4 +7,12 @@ app.get('/', (req, res) => {
   res.render('index', { name: 'Furkan', surname: 'Kaya' })
 })
 
+app.get('/home', (req, res) => {
+  res.render('home')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
 app.listen(3000, () => console.log('Server up!'))
