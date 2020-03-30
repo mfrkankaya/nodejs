9aef2ab8371b376b98359f8f4f5abfb8c2f3bd76
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-30 22:42:52 +0300

    Express public directory

--- express/app.js
+++ express/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'pug')
+app.use(express.static('public'))
+// app.use(express.static('another-public')) -> We can use more than one public directory
 
 app.get('/', (req, res) => {
   res.render('index', { name: 'Furkan', surname: 'Kaya' })
