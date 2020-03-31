8826a2cdee2b0f03be290e8bb6a1142ea503d72a
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-31 22:38:20 +0300

    Remove old promise code

--- movieapi/routes/movies.js
+++ movieapi/routes/movies.js
@@ -6,13 +6,7 @@ const Movie = require('../models/Movie')
 
 router.post('/', async (req, res, next) => {
   const movie = new Movie(req.body)
-
-  // movie.save((err, data) => {
-  //   if (err) res.json(err)
-
-  //   res.json({ status: true })
-  // })
-
+  
   try {
     await movie.save()
     res.json({ status: true })
