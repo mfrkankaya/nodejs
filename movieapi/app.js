6a55ac61de708bcba7d417014a89e3a61309f7c3
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-31 22:07:54 +0300

    Movies initial route

--- movieapi/app.js
+++ movieapi/app.js
@@ -8,7 +8,7 @@ const logger = require('morgan')
 require('./helpers/db')()
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const moviesRouter = require('./routes/movies')
 
 const app = express()
 
@@ -23,7 +23,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/api/movies', moviesRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
