aa02159df06be405e624b2b9da557cb43f409584
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-01 00:27:57 +0300

    Register user /register

--- movieapi/routes/index.js
+++ movieapi/routes/index.js
@@ -1,9 +1,24 @@
+const bcrypt = require('bcrypt')
 const express = require('express')
 const router = express.Router()
+
+const User = require('../models/User')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' })
+})
+
+router.post('/register', async (req, res, next) => {
+  const { username, password } = req.body
+
+  try {
+    const hashed = await bcrypt.hash(password, 10)
+    await new User({ username, password: hashed }).save()
+    res.json({ status: true })
+  } catch (error) {
+    res.json({ status: false, error })
+  }
 })
 
 module.exports = router
