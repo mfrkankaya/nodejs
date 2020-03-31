132d0a621bc253346557a6974c3916fb4fe3ced5
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-31 22:14:30 +0300

    POST Movie - change MOvies.js to Movie.js

--- movieapi/routes/movies.js
+++ movieapi/routes/movies.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET users listing. */
+// Models
+const Movie = require('../models/Movie')
+
+router.post('/', (req, res, next) => {
+  const movie = new Movie(req.body)
+
+  movie.save((err, data) => {
+    if (err) res.json(err)
+
+    res.json({ status: true })
+  })
+})
+
 router.get('/', (req, res, next) => {
   res.send({ example: 'movie router' })
 })
