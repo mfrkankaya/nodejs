7bc001c1cb96f024e1ac5a1995581fe71b77c1da
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-03-31 21:52:24 +0300

    Movie api initialized and mongo connected

---
+++ movieapi/helpers/db.js
@@ -0,0 +1,11 @@
+// mongodb://mfurkankaya:mf133256+@ds149894.mlab.com:49894/heroku_8k2fqqpv
+const mongoose = require('mongoose')
+
+module.exports = () => {
+  mongoose.connect('mongodb://mfurkankaya:mf133256@ds149894.mlab.com:49894/heroku_8k2fqqpv', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  mongoose.connection.on('open', () => console.log('Mongo connected.'))
+  mongoose.connection.on('error', () => console.log('Mongo failed.'))
+}
