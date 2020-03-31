1041ef263670a4229fe7a209ddf1569b5f3c1976
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-01 00:09:28 +0300

    GET directors/:directorId

--- movieapi/routes/directors.js
+++ movieapi/routes/directors.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 
@@ -43,11 +44,61 @@ router.get('/', async (req, res) => {
         }
       },
       {
-        $project: {
+        $project: {
           _id: '$_id._id',
           fullname: '$_id.fullname',
           bio: '$_id.bio',
-          movies: '$movies',
+          movies: '$movies'
+        }
+      }
+    ])
+    res.json({ status: true, data })
+  } catch (error) {
+    res.json({ status: false, error })
+  }
+})
+
+router.get('/:directorId', async (req, res) => {
+  const { directorId } = req.params
+  console.log(directorId);
+  
+  try {
+    const data = await Director.aggregate([
+      {
+        $match: {
+          _id: mongoose.Types.ObjectId(directorId)
+        }
+      },
+      {
+        $lookup: {
+          from: 'movies',
+          localField: '_id',
+          foreignField: 'directorId',
+          as: 'movies'
+        }
+      },
+      {
+        $unwind: {
+          path: '$movies',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $group: {
+          _id: {
+            _id: '$_id',
+            fullname: '$fullname',
+            bio: '$bio'
+          },
+          movies: { $push: '$movies' }
+        }
+      },
+      {
+        $project: {
+          _id: '$_id._id',
+          fullname: '$_id.fullname',
+          bio: '$_id.bio',
+          movies: '$movies'
         }
       }
     ])
