507f97b4873cd03d9cb0a970de19c0fd13958981
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-01 00:19:34 +0300

    Model Schema Validations

--- movieapi/models/Movie.js
+++ movieapi/models/Movie.js
@@ -5,12 +5,22 @@ const MovieSchema = new Schema({
   directorId: Schema.Types.ObjectId,
   title: {
     type: String,
-    required: true
+    required: [true, '`{PATH}` field is required.'],
+    maxlength: [15, '`{PATH}` field can have max ({MAXLENGTH}) character.'],
+    minlength: [1, '`{PATH}` field can have min ({MINLENGTH}) character.']
   },
   category: String,
   country: String,
-  year: Number,
-  imdb: Number,
+  year: {
+    type: Number,
+    max: 2040,
+    min: 1900
+  },
+  imdb: {
+    type: Number,
+    max: 10,
+    min: 0
+  },
   createdAt: {
     type: Date,
     default: Date.now
