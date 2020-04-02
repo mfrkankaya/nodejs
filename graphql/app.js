8ffeca8b62a1bb582c39ea2d2b6668688b7f7711
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-03 01:32:30 +0300

    Graphql | GraphQL Setup

--- graphql/app.js
+++ graphql/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql')
 
 const app = express()
+
+app.use('/graphql', expressGraphQL({
+  
+}))
 
 const onServerup = () => console.log('Server is up!')
 app.listen(3000, onServerup)
