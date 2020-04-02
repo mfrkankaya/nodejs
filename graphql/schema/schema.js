2d1705072c84ce300edb6f4fe8b52efd5955c282
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-03 01:47:37 +0300

    Graphql | resolve function

--- graphql/schema/schema.js
+++ graphql/schema/schema.js
@@ -1,6 +1,31 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = graphql
+
+const movies = [
+  {
+    id: '1',
+    title: 'God Father',
+    year: 1972,
+    description:
+      'Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak.'
+  },
+  {
+    id: '2',
+    title: 'Scarface',
+    year: 1980,
+    description:
+      'Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak.'
+  },
+  {
+    id: '3',
+    title: 'Babam ve Oğlum',
+    year: 2000,
+    description:
+      'Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak.'
+  }
+]
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -19,7 +44,15 @@ const RootQuery = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        // get data
+        /* wen we make a request like;
+        movie (id: 2){
+          title,
+          year
+        }
+        
+        args.id becomes 2
+        */
+        return _.find(movies, { id: args.id })
       }
     }
   }
