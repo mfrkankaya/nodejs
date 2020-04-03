198287fb1b5daa2489a879227f956c3cd2bd3a11
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-03 23:13:03 +0300

    createUser Mutation - 4 bcrypt hash

--- easysnap/models/User.js
+++ easysnap/models/User.js
@@ -1,20 +1,32 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
   username: {
     type: String,
     unique: true,
-    required: true,
+    required: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   createdAt: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
+})
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  bcrypt.hash(this.password, 10).then((hash) => {
+    this.password = hash
+    next()
+  })
 })
 
 module.exports = mongoose.model('user', UserSchema)
