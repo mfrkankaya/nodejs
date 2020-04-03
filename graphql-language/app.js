79be9a932acd7ab4bbfc07937458ad08eed78964
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-03 19:53:49 +0300

    Data Types

--- graphql-language/app.js
+++ graphql-language/app.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
+// String, Int, ID, Booleand, Float
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
+    id: ID!
     name: String!
     surname: String!
     age: Int
+    isAdmin: Boolean
+    score: Float
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
+    id: () => 'Fdsagfsgsadf',
     name: () => 'Furkan',
     surname: () => 'Kaya',
-    age: () => 24
+    age: () => 24,
+    isAdmin: () => true,
+    score: () => 123.3431
   },
 };
 
