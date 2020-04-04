032f4bb34a54fa72a6c7f3d38c595767d5639dfa
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-04 15:21:25 +0300

    Apollo setup

--- easysnap/client/src/index.js
+++ easysnap/client/src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({ uri: 'hhtp://localhost:300/graphql' })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
