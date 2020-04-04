07992ef866de33c605d41a94da3e53b5c89e3860
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-04 15:24:28 +0300

    Better syntax

--- easysnap/client/src/App.js
+++ easysnap/client/src/App.js
@@ -1,61 +1,58 @@
 import React from 'react'
 
 function App() {
-  return <div id="app">
-  <div className="container">
-      <div className="header">
-          <div className="logo">
-              <h2 className="logo__title">easysnap</h2>
+  return (
+    <div id='app'>
+      <div className='container'>
+        <div className='header'>
+          <div className='logo'>
+            <h2 className='logo__title'>easysnap</h2>
           </div>
 
-          <div className="header_menu">
-              <div className="active">
-                  snaps
-              </div>
-              <div>
-                  login
-              </div>
-              <div>
-                  join
-              </div>
+          <div className='header_menu'>
+            <div className='active'>snaps</div>
+            <div>login</div>
+            <div>join</div>
           </div>
-      </div>
+        </div>
 
-      <div className="description">
-          <p className="sub_header__desc">simple snap app with <span>react</span>.</p>
-      </div>
+        <div className='description'>
+          <p className='sub_header__desc'>
+            simple snap app with <span>react</span>.
+          </p>
+        </div>
 
-      <div>
+        <div>
           <form>
-              <input className="add-snap__input" type="text" placeholder="add snap" />
+            <input className='add-snap__input' type='text' placeholder='add snap' />
           </form>
-      </div>
-      <div>
-          <ul className="snaps">
-              <li>
-                  <div className="title">Lorem ipsum dolor sit amet</div>
-                  <div className="date">
-                      <span>now</span>
-                  </div>
-              </li>
-              <li>
-                  <div className="title">Curabitur gravida arcu ac tortor dignissim.</div>
-                  <div className="date">
-                      <span>5 minutes ago</span>
-                  </div>
-              </li>
-              <li>
-                  <div className="title">Tristique risus nec feugiat in fermentum.</div>
-                  <div className="date">
-                      <span>7 minutes ago</span>
-                  </div>
-              </li>
+        </div>
+        <div>
+          <ul className='snaps'>
+            <li>
+              <div className='title'>Lorem ipsum dolor sit amet</div>
+              <div className='date'>
+                <span>now</span>
+              </div>
+            </li>
+            <li>
+              <div className='title'>Curabitur gravida arcu ac tortor dignissim.</div>
+              <div className='date'>
+                <span>5 minutes ago</span>
+              </div>
+            </li>
+            <li>
+              <div className='title'>Tristique risus nec feugiat in fermentum.</div>
+              <div className='date'>
+                <span>7 minutes ago</span>
+              </div>
+            </li>
           </ul>
+        </div>
+        <div className='counter'>3 snap(s)</div>
       </div>
-      <div className="counter">3 snap(s)</div>
-  </div>
-</div>
-
+    </div>
+  )
 }
 
 export default App
