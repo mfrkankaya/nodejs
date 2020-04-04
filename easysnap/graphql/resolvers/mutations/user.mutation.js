497fed2dbc7ffbf578582171e3c2f13e5c13dbdc
Muhammed Furkan KAYA <muhammedfurkankaya@Muhammed-MacBook-Pro.local>
2020-04-04 15:00:11 +0300

    signIn Mutation

--- easysnap/graphql/resolvers/mutations/user.mutation.js
+++ easysnap/graphql/resolvers/mutations/user.mutation.js
@@ -1,7 +1,19 @@
+const bcrypt = require('bcrypt')
+
 module.exports = {
   createUser: async (parent, { data: { username, password } }, { User }) => {
     const user = await User.findOne({ username })
     if (user) throw new Error('Username already exists.')
     return await new User({ username, password }).save()
+  },
+
+  signIn: async (parent, { data: { username, password } }, { User }) => {
+    const user = await User.findOne({ username })
+    if (!user) throw new Error('User does not exists.')
+
+    const validPasword = await bcrypt.compare(password, user.password)
+    if (!validPasword) throw new Error('Wrong password.')
+
+    return { token: 'Test token' }
   }
 }
